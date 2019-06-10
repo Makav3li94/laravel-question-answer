5cfcce2095ae899da2275206bea211f2db8697e6
MAKAVELI <neogood@yahoo.com>
2019-06-10 19:22:15 +0430

    add new Question works

--- resources/views/questions/create.blade.php
+++ resources/views/questions/create.blade.php
@@ -25,20 +25,20 @@
                 @csrf
                     <div class="form-group" >
                         <label for="question-title">Question Title</label>
-                        <input type="text" name="title" id="question-title" class="form-control {{$errors->has('title') ? 'is-valid' : ''}}">
+                        <input type="text" name="title" value="{{old('title')}}" id="question-title" class="form-control {{$errors->has('title') ? 'is-invalid' : ''}}">
                         @if($errors->has('title'))
                             <div class="invalid-feedback">
-                                <strong>{{$error->first('title')}}</strong>
+                                {{$errors->first('title')}}
                             </div>
                         @endif
                     </div>
 
                     <div class="form-group" >
                         <label for="question-body">Explain for Question</label>
-                        <textarea name="body" id="question-body" cols="30" rows="10" class="form-control {{$errors->has('body') ? 'is-valid' : ''}}"></textarea>
+                        <textarea name="body" id="question-body" cols="30" rows="10" class="form-control {{$errors->has('body') ? 'is-invalid' : ''}}">{{old('body')}}</textarea>
                         @if($errors->has('body'))
                             <div class="invalid-feedback">
-                                <strong>{{$error->first('body')}}</strong>
+                                {{$errors->first('body')}}
                             </div>
                         @endif
                     </div>
