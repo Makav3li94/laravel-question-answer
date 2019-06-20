49fd7efd8f3e3e4bcfadd1df98fe86c076ca82bf
MAKAVELI <neogood@yahoo.com>
2019-06-20 13:45:32 +0430

    votes up , down facvorted mark up added

--- resources/views/questions/show.blade.php
+++ resources/views/questions/show.blade.php
@@ -5,40 +5,58 @@
     <div class="row justify-content-center">
         <div class="col-md-12">
             <div class="card">
-                <div class="card-header">
-                <div class="d-flex align-items-center">
-                    <h1> 
-                    {{$question->title}}
-                    </h1>
-                    <div  class="ml-auto" >
-                        <a href="{{route('questions.index')}}" class="btn btn-outline-secondary"> 
-                            Back to all questions
-                        </a>
-                    </div>
-                </div>
-                
-                </div>
-
                 <div class="card-body">
-
-                    {!! $question->body_html !!}
-
-                    <div class="float-right  mt-4">
-                        <span class="text-muted">
-                            Asked By {{$question->created_date}}
-                        </span>
-                        <div class="media mt-2">
-                            <a href="{{$question->user->url}}" class="pr-2">
-                                <img src="{{$question->user->avatar}}" alt="">
-                            </a>
-                            <div class="media-body">
-                            <a href="{{$question->user->url}}" class="pr-2">
-                            {{$question->user->name}}
-                            </a>
+                    <div class="card-title">
+                        <div class="d-flex align-items-center">
+                            <h1>
+                                {{$question->title}}
+                            </h1>
+                            <div class="ml-auto">
+                                <a href="{{route('questions.index')}}" class="btn btn-outline-secondary">
+                                    Back to all questions
+                                </a>
                             </div>
                         </div>
+
                     </div>
-                
+                    <hr>
+                    <div class="media">
+                        <div class="d-flex flex-column vote-controls">
+                            <a title="this question is userful" class="vote-up">
+                               <i class='fa fa-caret-up fa-3x'></i>
+                            </a>
+                            <span class="votes-count">122</span>
+                            <a title="This question is not useful" class="vote-down off">
+                            <i class='fa fa-caret-down fa-3x'></i>
+                            </a>
+                            <a title="Click to mark as favorite question (Click again to undo)" class="favorite mt-2 favorited">
+                            <i class='fa fa-star fa-2x'></i>
+                            
+                            </a>
+                            <span class="favorites-count">123</span>
+                        </div>
+                        <div class="media-body">
+
+                            {!! $question->body_html !!}
+
+                            <div class="float-right  mt-4">
+                                <span class="text-muted">
+                                    Asked By {{$question->created_date}}
+                                </span>
+                                <div class="media mt-2">
+                                    <a href="{{$question->user->url}}" class="pr-2">
+                                        <img src="{{$question->user->avatar}}" alt="">
+                                    </a>
+                                    <div class="media-body">
+                                        <a href="{{$question->user->url}}" class="pr-2">
+                                            {{$question->user->name}}
+                                        </a>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+
+                    </div>
                 </div>
             </div>
         </div>
@@ -54,27 +72,41 @@
                         </h2>
                         <hr>
                         @foreach($question->answers as $answer)
-                            <div class="media">
-                                <div class="media-body">
-                                 {!! $answer->body !!}
-                                    <div class="float-right  mt-4">
-                                        <span class="text-muted">
-                                            Answered {{$answer->created_date}}
-                                        </span>
-                                        <div class="media mt-2">
+                        <div class="media">
+                        <div class="d-flex flex-column vote-controls">
+                            <a title="this question is userful" class="vote-up">
+                               <i class='fa fa-caret-up fa-3x'></i>
+                            </a>
+                            <span class="votes-count">122</span>
+                            <a title="This question is not useful" class="vote-down off">
+                            <i class='fa fa-caret-down fa-3x'></i>
+                            </a>
+                            <a title="Mark this answer as best answer" class="vote-accepted mt-2 ">
+                            <i class='fa fa-check fa-2x'></i>
+                            
+                            </a>
+                            <span class="favorites-count">123</span>
+                        </div>
+                            <div class="media-body">
+                                {!! $answer->body !!}
+                                <div class="float-right  mt-4">
+                                    <span class="text-muted">
+                                        Answered {{$answer->created_date}}
+                                    </span>
+                                    <div class="media mt-2">
+                                        <a href="{{$answer->user->url}}" class="pr-2">
+                                            <img src="{{$answer->user->avatar}}" alt="">
+                                        </a>
+                                        <div class="media-body">
                                             <a href="{{$answer->user->url}}" class="pr-2">
-                                                <img src="{{$answer->user->avatar}}" alt="">
+                                                {{$answer->user->name}}
                                             </a>
-                                            <div class="media-body">
-                                            <a href="{{$answer->user->url}}" class="pr-2">
-                                            {{$answer->user->name}}
-                                            </a>
-                                            </div>
                                         </div>
                                     </div>
                                 </div>
                             </div>
-                            <hr>
+                        </div>
+                        <hr>
                         @endforeach
                     </div>
                 </div>
